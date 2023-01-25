159628ae1fcc9496037e1429fb6eb27cbfe4189f
pablotroncosog <pablo.troncoso@mail.udp.cl>
2023-01-25 22:47:28 +0000

    main

--- pag/src/components/Navbar.js
+++ pag/src/components/Navbar.js
@@ -18,9 +18,7 @@ export function Navbar() {
                     <button className="btn btn-warning text-dark dropdown-toggle" type="button" data-bs-toggle="dropdown">
                         Favoritos</button>
                     <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                       
                     </ul>
                 </div>
             </div>
